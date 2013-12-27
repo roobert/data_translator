2593ef3cb31dfab346253255ca0ffc3be3f7448b
Rob Wilson <roobert@gmail.com>
2013-12-27 13:53:29 +0000

    renamed switch_config_parser

--- bin/test.rb
+++ bin/test.rb
@@ -2,7 +2,7 @@
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../lib'))
 
-require 'switch_config_parser'
+require 'switch_data_parser'
 require 'towser'
 require 'awesome_print'
 require 'yaml'
@@ -17,8 +17,8 @@ DATA_DIR = File.join(File.dirname(__FILE__), '../data')
 # * see which switches each interface is plugged into
 
 # TODO: add snmp data parser..
-switch_config = SwitchConfigParser::Regexp::Config.parse(IO.readlines(File.join(DATA_DIR, "switch.config")))
-bridge_table  = SwitchConfigParser::Regexp::BridgeTable.parse(IO.readlines(File.join(DATA_DIR, "bridge_table.config")))
+switch_config = SwitchDataParser::Regexp::Config.parse(IO.readlines(File.join(DATA_DIR, "switch.config")))
+bridge_table  = SwitchDataParser::Regexp::BridgeTable.parse(IO.readlines(File.join(DATA_DIR, "bridge_table.config")))
 
 # machine config is already a hash so need to run through a parser first, although we could do with improving mcollective plugin to output proper json / yaml..
 #machines = Machines.new(YAML.load_file('../data/mcollective.config'))
