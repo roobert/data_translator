fb73e676a9345bbd6289ef01fdb62a2c4f1e9309
Rob Wilson <roobert@gmail.com>
2013-12-27 13:38:18 +0000

    reorganised dirs

---
+++ lib/towser/network/switch/bridge_table.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+module Towser
+  class Network
+    class Switch
+      class BridgeTable
+        attr_reader :entries
+
+        def initialize(table)
+          @table   = table
+          @entries = {}
+
+          self.objectify
+        end
+
+        def objectify
+          @table.each do |identifier, entry|
+            @entries[identifier] = Towser::Network::Switch::BridgeTable::Entry.new(identifier, entry)
+          end
+        end
+
+        def find_macs_for_port(stack_member, unit, port)
+          entries.each do |identifier, entry|
+            if entry.stack_member == stack_member and entry.unit == unit and entry.port == port
+              return entry.vlans.vlans
+            end
+          end
+          return
+        end
+
+        def to_hash
+          {
+            :entries => @entries.to_hash
+          }
+        end
+
+        alias_method :inspect, :to_hash
+        alias_method :to_s, :to_hash
+      end
+    end
+  end
+end
